ba9660f07e769198db9ea84f69704ac767e43247
rupam2001 <rjdasgalexy@gmail.com>
2021-09-14 19:19:59 +0530

    db models edited

--- database/models/crime_reports.js
+++ database/models/crime_reports.js
@@ -1,4 +1,5 @@
 var mongoose = require("mongoose");
+const locationSchema = require("./sub_schemas/location.schema");
 var Schema = mongoose.Schema;
 var crime_reports = new Schema({
   type: {
@@ -6,16 +7,18 @@ var crime_reports = new Schema({
     required: true,
   },
   geo_location: {
-    type: String,
+    type: locationSchema,
     required: true,
   },
   brief_report: {
     type: String,
   },
-  media_url: {
-    type: String,
-    required: true,
-  },
+  media_urls: [
+    {
+      type: String,
+      required: true,
+    },
+  ],
 });
 
 const CRIME_REPORTS = mongoose.model("crime_reports", crime_reports);
--- database/models/donation.js
+++ database/models/donation.js
@@ -1,39 +1,52 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
-var donation = new Schema({
-  donor: {
-    type: Schema.Types.ObjectId,
-    ref: "general_users",
+var donation = new Schema(
+  {
+    donor: {
+      type: Schema.Types.ObjectId,
+      ref: "general_users",
+    },
+    donor_name: {
+      type: String,
+    },
+    contact_number: {
+      type: Number,
+    },
+    ngo: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: "ngo",
+    },
+    is_accepted_by_ngo: {
+      type: Boolean,
+      default: false,
+    },
+    is_donation_received: {
+      type: Boolean,
+      default: false,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    media_url: [
+      {
+        type: String,
+        required: true,
+      },
+    ],
+    dimensions: [{ type: Number, required: true }],
+    weight: {
+      type: Number,
+      required: true,
+    },
+    time_stamp: {
+      type: String,
+      required: true,
+    },
   },
-  donor_name: {
-    type: String,
-  },
-  contact_number: {
-    type: Number,
-  },
-  ngo: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: "ngo",
-  },
-  type: {
-    type: String,
-    required: true,
-  },
-  media_url: {
-    type: String,
-    required: true,
-  },
-  dimensions: [{ type: Number, required: true }],
-  weight: {
-    type: Number,
-    required: true,
-  },
-  time_stamp: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const DONATION = mongoose.model("donation", donation);
 module.exports = DONATION;
