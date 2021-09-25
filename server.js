c50dc2a30f3e0fb5545cc87b1575c2a6817f59dc
Devzard <debashishgogoi999@gmail.com>
2021-09-25 17:41:07 +0530

    asc desc

--- server.js
+++ server.js
@@ -63,9 +63,9 @@ app.use(
 app.use("/api/homeless/data/", require("./routes/homeless/homeless"));
 app.use("/api/crime/data/", require("./routes/crime-report/crimeData"));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/public/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname + "/public/index.html"));
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(">> Server is running at port", PORT));
